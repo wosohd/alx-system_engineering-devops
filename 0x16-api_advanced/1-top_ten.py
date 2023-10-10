e519ab46765e79394ccf11ff5c92f4dbe7dd6520
wosohd <wosohd@gmail.com>
2023-10-10 11:30:46 -0700

    advanced API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Prints a hot post on a given Reddit subreddit"""
+
+from requests import get
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit"""
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
